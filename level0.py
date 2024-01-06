e3c0787d4d9cf112d60ffe8733daf7d3fffff64d
VIVI0007 <154956779+VIVI0007@users.noreply.github.com>
2024-01-06 11:53:43 +0530

    Update level0.py

--- level0.py
+++ level0.py
@@ -1,6 +1,42 @@
-
-import numpy as np
 import json
+from typing import List
+
+def parse_input(input_data):
+    distances = {}
+    neighborhoods = input_data.get("neighbourhoods", {})
+    for n_id, n_info in neighborhoods.items():
+        distances[n_id] = n_info["distances"]
+    restaurants = input_data.get("restaurants",{})
+    for r_id, r_info in restaurants.items():
+        distances[r_id] = r_info["neighbourhood_distance"]
+    return distances
+def nearest_neighbor(cities: List[str], distances: dict, start) -> List[str]:
+    unvisited = set(cities)
+    current = start
+    if current in unvisited:
+        unvisited.remove(current)
+    tour = [current]
+    while unvisited:
+        current_index = cities.index(current)
+        next_city = min(unvisited, key=lambda city: distances[current_index][cities.index(city)])
+        tour.append(next_city)
+        unvisited.remove(next_city)
+        current = next_city
+    if start not in tour:
+        tour.append(start)
+    start_index = tour.index(start)
+    tour = tour[start_index:] + tour[:start_index + 1]
+    return tour
+
+def format_output(tsp_path):
+    output = {"v0": {"path": tsp_path}}
+    return output
+
+
+
+#input_file_path = "C://Users//TEMP.CS2K16//Downloads//level0.json"
+#with open(input_file_path, 'r') as f:
+#    input_data = json.load(f)
 with open('C://Users//TEMP.CS2K16//Downloads//level0.json') as user_file:
   file_contents = user_file.read()
   
@@ -8,67 +44,12 @@ with open('C://Users//TEMP.CS2K16//Downloads//level0.json') as user_file:
 parsed_json = json.loads(file_contents)
 #print(type(parsed_json))
 
-json_data = parsed_json
+input_data = parsed_json
 
-#for key, value in parsed_json.iteritems():
-#    if key=="n_neighbourhoods":
-#        print(key, ":", value)
+start_node = "r0"
 
-print(my_dict['vehicles']['v0']['start_point'])     #prints r0
+distance_matrix = parse_input(input_data)
+tsp_path = nearest_neighbor(list(distance_matrix.keys()), list(distance_matrix.values()), start_node)
+output_data = format_output(tsp_path)
 
-
-from sys import maxsize
-from itertools import permutations
-
-V = int(my_dict['n_neighbourhoods']) + int(my_dict['n_restaurants'])
-
-# implementation of traveling Salesman Problem
-def shortestpath(graph, s):
-
-    # store all vertex apart from source vertex
-    vertex = []
-    for i in range(V):
-        if i != s:
-            vertex.append(i)
-
-    # store minimum weight Hamiltonian Cycle
-    min_path = maxsize
-    next_permutation = permutations(vertex)
-    path = []
-
-    for i in next_permutation:
-
-        # store current Path weight(cost)
-        current_pathweight = 0
-        current_path = [s]
-
-        # compute current path weight
-        k = s
-        for j in i:
-            current_pathweight += graph[k][j]
-            current_path.append(j)
-            k = j
-        current_pathweight += graph[k][s]
-        current_path.append(s)
-
-        # update minimum
-        if current_pathweight < min_path:
-            min_path = current_pathweight
-            path = current_path
-
-    print("Minimum Weight Hamiltonian Cycle:", path)
-    return min_path
-
-s=0
-graph=[]
-for neighborhood, details in my_dict["restaurants"].items():
-    graph.extend(details["neighbourhood_distance"])
-for neighborhood, details in my_dict["neighbourhoods"].items():
-    all_distances = []
-    all_distances.extend(details["distances"])
-    graph.append(all_distances)
-min_weight = shortestpath(graph, s)
-print("Minimum Weight:", min_weight)
-
-
-
+print(output_data)
